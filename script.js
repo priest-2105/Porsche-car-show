7cc996533677f5e34ac333fed03cb832419e0159
priest-2105 <fawzybailey782@gmail.com>
2023-07-13 09:03:04 +0100

    text animation

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+// // Number scryIdoll
+
+// // intializing the number with id 'animated-number' in a variable 
+
+// const animatedNumber = document.getElementById('animated-number');
+
+// const targetNumberone = 1959; //Number im triny to animate
+// const targetNumbertwo = 1979; //Number im triny to animate
+// const targetNumberthree = 1989; //Number im triny to animate
+// const targetNumberfour = 1999; //Number im triny to animate
+// const duration= 3000; //Duration of the animation in Millisecond
+// const framesPerSecond = 30; //Number of frames persecond
+
+
+// const totalFrames = Math.ceil(framesPerSecond * (duration / 1000));
+
+// const incrementone = targetNumberone / (duration / totalFrames );
+// // const incrementtwo = targetNumbertwo / (duration / totalFrames );
+// // const incrementthree = targetNumberthree / (duration / totalFrames );
+// // const incrementfour = targetNumberfour / (duration / totalFrames );
+
+
+// let currentNumber = 1909 ;
+
+
+// function updateNumber(){
+
+//     currentNumber += incrementone ;
+//     animatedNumber.textContent = Math.floor(currentNumber);
+
+//     if(currentNumber <= targetNumberone){
+//         requestAnimationFrame(updateNumber);
+//     }
+
+//     // if(targetNumberone <= targetNumbertwo){
+//     //     requestAnimationFrame(updateNumber);
+//     // }
+
+    
+//     // if(targetNumbertwo <= targetNumberthree){
+//     //     requestAnimationFrame(updateNumber);
+//     // }
+
+    
+//     // if(targetNumberthree <= targetNumberfour){
+//     //     requestAnimationFrame(updateNumber);
+//     // }
+
+
+
+
+// } updateNumber();
+
